941b572fb549a33d9179038f5c76a0b37169f91d
McPenquen <majda.calk@outlook.com>
2021-10-26 20:39:49 +0200

    Running cuda

--- labs/lab04/my-queue.cpp
+++ labs/lab04/my-queue.cpp
@@ -2,6 +2,20 @@
 #include <thread>
 #include <chrono>
 #include <cmath>
+#include <queue>
 
 using namespace std;
 using namespace std::chrono;
+
+struct MyQueue
+{
+public:
+private:
+	queue<string> _storage;
+};
+
+int main(int argc, char** argv)
+{
+
+	return 0;
+}
